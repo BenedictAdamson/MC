06503814ea0285c92d6b1f260a40f2c716abc64a
Benedict Adamson <badamson@spamcop.net>
2020-12-02 14:47:36 +0000

    Add model class UserGameAssociation

---
+++ MC-model/src/main/java/uk/badamson/mc/UserGameAssociation.java
@@ -0,0 +1,139 @@
+package uk.badamson.mc;
+/*
+ * © Copyright Benedict Adamson 2020.
+ *
+ * This file is part of MC.
+ *
+ * MC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MC.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * <p>
+ * A reified association between a {@linkplain User user} and a
+ * {@linkplain Game}, with the user and game referred to using their unique IDs.
+ * </p>
+ * <p>
+ * This class is intended to be used only for recording associations in the
+ * repository layer.
+ * </p>
+ */
+@Immutable
+public final class UserGameAssociation {
+
+   private final UUID user;
+   private final Game.Identifier game;
+
+   /**
+    * <p>
+    * Construct an object with given attribute values.
+    * </p>
+    * <ul>
+    * <li>The {@linkplain #getUser() user ID} of this object is the same as the
+    * given user ID.</li>
+    * <li>The {@linkplain #getGame() game ID} of this object is the same as the
+    * given game ID.</li>
+    * </ul>
+    *
+    * @param user
+    *           The {@linkplain User#getId() unique ID} of the {@linkplain User
+    *           user} associated with the game.
+    * @param game
+    *           The {@linkplain Game#getIdentifier() unique ID} of the
+    *           {@linkplain Game game} associated with the user.
+    * @throws NullPointerException
+    *            <ul>
+    *            <li>If {@code user} is null.</li>
+    *            <li>If {@code game} is null.</li>
+    *            </ul>
+    */
+   @JsonCreator
+   public UserGameAssociation(@Nonnull @JsonProperty("user") final UUID user,
+            @Nonnull @JsonProperty("game") final Game.Identifier game) {
+      this.user = Objects.requireNonNull(user, "user");
+      this.game = Objects.requireNonNull(game, "game");
+   }
+
+   /**
+    * <p>
+    * Whether this object is <i>equivalent</i> to another object.
+    * </p>
+    * <p>
+    * The {@link UserGameAssociation} class has <i>value semantics</i>: this
+    * object is equivalent to another only if the other object is also a
+    * {@link UserGameAssociation} and they have equivalent attribute values.
+    */
+   @Override
+   public boolean equals(final Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (!(obj instanceof UserGameAssociation)) {
+         return false;
+      }
+      final var other = (UserGameAssociation) obj;
+      return user.equals(other.user) && game.equals(other.game);
+   }
+
+   /**
+    * <p>
+    * The {@linkplain Game#getIdentifier() unique ID} of the {@linkplain Game
+    * game} associated with the user.
+    * </p>
+    * <ul>
+    * <li>Non null</li>
+    * </ul>
+    *
+    * @return the user ID
+    */
+   @Nonnull
+   public Game.Identifier getGame() {
+      return game;
+   }
+
+   /**
+    * <p>
+    * The {@linkplain User#getId() unique ID} of the {@linkplain User user}
+    * associated with the game.
+    * </p>
+    * <ul>
+    * <li>Non null</li>
+    * </ul>
+    *
+    * @return the user ID
+    */
+   @Nonnull
+   public UUID getUser() {
+      return user;
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(game, user);
+   }
+
+   @Override
+   public String toString() {
+      return "(" + user + ", " + game + ")";
+   }
+
+}
---
+++ MC-model/src/test/java/uk/badamson/mc/UserGameAssociationTest.java
@@ -0,0 +1,187 @@
+package uk.badamson.mc;
+/*
+ * © Copyright Benedict Adamson 2020.
+ *
+ * This file is part of MC.
+ *
+ * MC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MC.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p>
+ * Unit tests and auxiliary test code for the {@link UserGameAssociation} class
+ */
+public class UserGameAssociationTest {
+
+   @Nested
+   public class Constructor {
+
+      @Nested
+      public class Duplicates {
+
+         @Test
+         public void a() {
+            test(USER_A, GAME_A);
+         }
+
+         @Test
+         public void b() {
+            test(USER_B, GAME_B);
+         }
+
+         private void test(final UUID user, final Game.Identifier game) {
+            final var association1 = new UserGameAssociation(user, game);
+            // Tough test: equivalent but not same attribute objects
+            final var association2 = new UserGameAssociation(copy(user),
+                     copy(game));
+
+            assertInvariants(association1, association2);
+            assertEquals(association1, association2);
+         }
+      }// class
+
+      @Nested
+      public class TwoDifferent {
+
+         @Test
+         public void all() {
+            final var association1 = new UserGameAssociation(USER_A, GAME_A);
+            final var association2 = new UserGameAssociation(USER_B, GAME_B);
+
+            assertInvariants(association1, association2);
+            assertNotEquals(association1, association2);
+         }
+
+         @Test
+         public void game() {
+            final var association1 = new UserGameAssociation(USER_A, GAME_A);
+            final var association2 = new UserGameAssociation(USER_A, GAME_B);
+
+            assertInvariants(association1, association2);
+            assertNotEquals(association1, association2);
+         }
+
+         @Test
+         public void user() {
+            final var association1 = new UserGameAssociation(USER_A, GAME_A);
+            final var association2 = new UserGameAssociation(USER_B, GAME_A);
+
+            assertInvariants(association1, association2);
+            assertNotEquals(association1, association2);
+         }
+
+      }// class
+
+      @Test
+      public void a() {
+         test(USER_A, GAME_A);
+      }
+
+      @Test
+      public void b() {
+         test(USER_B, GAME_B);
+      }
+
+      private UserGameAssociation test(final UUID user,
+               final Game.Identifier game) {
+         final var association = new UserGameAssociation(user, game);
+
+         assertInvariants(association);
+         assertAll("Attribute values",
+                  () -> assertSame(user, association.getUser(), "user"),
+                  () -> assertSame(game, association.getGame(), "game"));
+         return association;
+      }
+
+   }// class
+
+   @Nested
+   public class Json {
+
+      @Test
+      public void a() {
+         test(USER_A, GAME_A);
+      }
+
+      @Test
+      public void b() {
+         test(USER_B, GAME_B);
+      }
+
+      private void test(final UUID user, final Game.Identifier game) {
+         final var association = new UserGameAssociation(user, game);
+
+         final var deserialized = JsonTest.serializeAndDeserialize(association);
+
+         assertInvariants(association, deserialized);
+         assertEquals(association, deserialized);
+      }
+   }// class
+
+   private static final UUID USER_A = UUID.randomUUID();
+
+   private static final UUID USER_B = UUID.randomUUID();
+
+   private static final Game.Identifier GAME_A = new Game.Identifier(
+            UUID.randomUUID(), Instant.EPOCH);
+
+   private static final Game.Identifier GAME_B = new Game.Identifier(
+            UUID.randomUUID(), Instant.now());
+
+   public static void assertInvariants(final UserGameAssociation association) {
+      ObjectTest.assertInvariants(association);// inherited
+
+      final var game = association.getGame();
+      assertAll("Not null", () -> assertNotNull(association.getUser(), "user"),
+               () -> assertNotNull(game, "game"));
+      GameTest.assertInvariants(game);
+   }
+
+   public static void assertInvariants(final UserGameAssociation association1,
+            final UserGameAssociation association2) {
+      ObjectTest.assertInvariants(association1, association2);// inherited
+      final var equals = association1.equals(association2);
+      assertAll("Value semantics requires equivalent attribute values",
+               () -> assertFalse(equals && !association1.getUser()
+                        .equals(association2.getUser()), "user"),
+               () -> assertFalse(equals && !association1.getGame()
+                        .equals(association2.getGame()), "game"));
+   }
+
+   private static Game.Identifier copy(final Game.Identifier id) {
+      return new Game.Identifier(copy(id.getScenario()), copy(id.getCreated()));
+   }
+
+   private static Instant copy(final Instant t) {
+      return Instant.ofEpochSecond(t.getEpochSecond(), t.getNano());
+   }
+
+   private static UUID copy(final UUID id) {
+      return new UUID(id.getMostSignificantBits(),
+               id.getLeastSignificantBits());
+   }
+}
